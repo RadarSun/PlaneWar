f455803f93cb404e4d28cc0bf1155365f3d7b252
RadarSun <hitsunzhicong@163.com>
2020-04-04 20:29:30 +0800

    planeWar V1.0 实现简单的射击以及碰撞检测

---
+++ heroplane.cpp
@@ -0,0 +1,6 @@
+#include "heroplane.h"
+
+heroplane::heroplane()
+{
+
+}
---
+++ heroplane.h
@@ -0,0 +1,11 @@
+#ifndef HEROPLANE_H
+#define HEROPLANE_H
+
+
+class heroplane
+{
+public:
+    heroplane();
+};
+
+#endif // HEROPLANE_H
